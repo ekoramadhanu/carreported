6e88a4b640e1dee01145dc6bca2dd8d98726c5c9
Eko Rama <ekorama97@gmail.com>
2019-05-05 10:27:00 +0700

    first commit

---
+++ asset/style.js
@@ -0,0 +1,11 @@
+function getSuplier(){
+    document.getElementById('frame').scr = "https://appslayananbi.herokuapp.com/public/dashboard/43dd9492-4405-480b-b7a6-03c5b2fb7372";
+}
+
+function getCustomer(){
+    document.getElementById('frame').scr = "http://appslayananbi.herokuapp.com/public/dashboard/730b2723-4407-4ad2-bce1-d808a2ebf945";    
+}
+
+function getTransaction(){
+    document.getElementById('frame').scr = "http://appslayananbi.herokuapp.com/public/dashboard/c029c9e6-0cec-410c-a8ee-890f05944197";    
+}
